538464f87b7b3865d7e36636e29ea11dec5b04e8
Revathi@003 <jrevathi301@gmail.com>
2023-06-09 15:50:51 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import SignIn from './Main';
 import Main from './Main';
 function App() {
   return (
     <div className="App">
+      <SignIn/>
       
-      <Main/>
     </div>
   );
 }
